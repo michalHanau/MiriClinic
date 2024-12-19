d34e7d781af8e27ae9128988b056593fbc34390a
michal hanau <mh3182100@gmail.com>
2024-12-19 21:14:40 +0200

    add client domain and build

--- server/server.js
+++ server/server.js
@@ -16,8 +16,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://miriclinic-client.onrender.com/', // דומיין הקליינט
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+  
 app.use('/api/appointments', appointmentsRoute);
 app.use('/api/customers', customersRoute);
 app.use('/api/treatments', treatmentsRoute);
